ab77f460131a0e0a49b26b56a8834ee470305aa6
Abdelwahad-ALLOUCH <allouchabdelwahad@gmail.com>
2024-10-04 23:45:09 +0100

    code changed

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+
 <?php 
 // require "../src/controllers/TaskController.php";
 require "../src/views/home.php";
 // 
-=======
-<?php
+
+
 // Prevent caching (use cautiously in production)
 header("Cache-Control: no-cache, must-revalidate");
 header("Pragma: no-cache");
@@ -18,4 +18,3 @@ define('BASE_CFG', realpath(__DIR__ . '/../config/'));
 
 require_once BASE_SRC . "/controllers/task.controller.php";
 require_once BASE_SRC . "/controllers/route.controller.php";
->>>>>>> c17974646f7f668dc9a5b4af12ef1b6abebda4ca
