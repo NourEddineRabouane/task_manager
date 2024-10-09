c5123e7abe3da21d094c64e1cde74e2b1977d9ef
Abdelmalek <hamdaabdo06@gmail.com>
2024-10-09 17:15:07 +0100

    msssssg

--- src/partials/navbar.php
+++ src/partials/navbar.php
@@ -5,7 +5,11 @@
     </div> -->
     <nav id="nav">
         <ul>
-            <li><a href="/">Home</a></li>
+            <li>
+                <div class = "icon-1>
+                    <a href="">H</a>
+                </div>
+            </li>
             <li><a href="/about">About</a></li>
             <li><a href="/dashboard">Dashboard</a></li>
         </ul>
