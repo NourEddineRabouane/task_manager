ca64b2e715df3f3705720d106513f1f897a67dbb
noureddine <rabouanenoureddine1@gmail.com>
2024-10-09 17:12:07 +0100

    added the daily tasks stuff

--- src/partials/navbar.php
+++ src/partials/navbar.php
@@ -5,8 +5,8 @@
     <nav id="nav">
         <ul>
             <li><a href="/"><i class="fa-solid fa-house"></i> Home</a></li>
-            <li><a href="/about"><i class="fa-solid fa-question"></i> About</a></li>
             <li><a href="/dashboard"><i class="fa-solid fa-chess-board"></i> Dashboard</a></li>
+            <li><a href="/dailytasks"><i class="fa-solid fa-chess-board"></i> dailytasks</a></li>
         </ul>
     </nav>
 </div>
